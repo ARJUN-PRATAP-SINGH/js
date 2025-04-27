6c5caf820024bff728617a66627336fffb1ec7a1
ARJUN-PRATAP-SINGH <arjunpratapsingh8393@gmail.com>
2025-04-27 22:11:28 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hellobuddy@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+
+// accountEmail = "hellobuddy@google.com"
+// accountPassword = "12345"
+// accountCity = "Jaipur"
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
